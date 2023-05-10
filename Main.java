b4c096a86964867c3fb3aea4d76617a0591b7316
Su <aas-nky-u1@fh-h.de>
2023-05-10 16:53:52 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -8,4 +8,12 @@
 
 public class Main
 {
+    public static void main(String args[])
+    {
+        int summe = 0;
+        for(int i = 0; i <= 10; i++)
+        {
+            summe += i;
+        }
+    }
 }
