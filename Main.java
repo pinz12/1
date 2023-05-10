89156768f59908950ae453f3306a61e0fdc64fda
Su <aas-nky-u1@fh-h.de>
2023-05-10 17:01:03 +0200

    Multiplikation  wurde umgesetzt

--- Main.java
+++ Main.java
@@ -15,5 +15,11 @@ public class Main
         {
             summe += i;
         }
+        
+        int mult = 1;
+        for(int i = 2; i <= 10; i++)
+        {
+            summe *= i;
+        }
     }
 }
